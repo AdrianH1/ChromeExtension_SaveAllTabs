0237a3641bc4a7c5f7b56d3bf89b12db5016d69a
AdrianHugentobler <adrian.hugentobler@hotmail.com>
2020-07-08 18:39:16 +0200

    Code refractor and adding Feature to create a Folder for each chrome window

--- background.js
+++ background.js
@@ -1,40 +1,89 @@
-WindowIDs();
-getFolders();
+createFolders();
 
-function WindowIDs(){
+function createFolders(){
     
-    let params = {
-        windowType: 'normal'
-    }
+    //Get Folders-----------------------------------------------------------
+    let parentId = '2';  
     
-    chrome.tabs.query(params, getWindowIDs);
-    
-    function getWindowIDs(tabs){
-        
-        let WindowID= [];
-        
-        for (i=0; i < tabs.length; i++){
-            if(!WindowID.includes(tabs[i].windowId)){
-                WindowID[WindowID.length] = tabs[i].windowId;
+    chrome.bookmarks.getChildren(parentId, folders);
+    function folders(tree){
+        console.log("---getFolders-Anfang");
+
+        let FolderID = [];
+
+        for(i=0; i < tree.length; i++){
+            if(!FolderID.includes(tree[i].id)){
+                FolderID[FolderID.length] = tree[i].id;
             }
         }
-        console.log(WindowID);
-    
-        let ValueWindowID = WindowID;
-        chrome.storage.local.set({keyWindowID: ValueWindowID}, function() {
-          console.log('Value is set to ' + ValueWindowID);
-        });
-   }
-}
 
-function getFolders(){
-    chrome.bookmarks.getTree(getTree);
-    
-    function getTree(tree){
-        console.log(tree);
-        
-        chrome.storage.local.get(['keyWindowID'], function(result) {
-          console.log(result.keyWindowID);
-        });
+        chrome.storage.local.set({keyFolderID: FolderID,}, function() {});
+        console.log("FolderID: " + FolderID);
+        console.log("FolderID Length: " + FolderID.length);
+        console.log("---getFolders-Ende");
+        //----------------------------------------------------------------------
+
+
+        //Get Windows-----------------------------------------------------------       
+            let params = {
+                windowType: 'normal'
+            }
+
+            chrome.tabs.query(params, getWindowIDs);
+            function getWindowIDs(tabs){
+                console.log("---getWindowID-Anfang");
+
+                let WindowID = [];
+                let TabsArray = new Array(tabs.length);
+                let InnerTabsArray = new Array(4);
+                let countFolderID = 0;
+                
+                for (i=0; i < tabs.length; i++){
+                    if(!WindowID.includes(tabs[i].windowId)){
+                        WindowID[WindowID.length] = tabs[i].windowId;   
+                    }
+
+                }
+
+                chrome.storage.local.set({keyWindowID: WindowID, keyTabsArray: TabsArray}, function() {});
+                console.log("WindowsID: " + WindowID);
+                console.log("WindowsID Length: " + WindowID.length);
+                console.log("---getWindowID-Ende");
+        //---------------------------------------------------------------------
+
+
+        //Create Folders/Bookmarks---------------------------------------------
+                    chrome.storage.local.get(['keyWindowID', 'keyFolderID', 'keyTabsArray'], function(result) {
+                    console.log("---createFolders-Anfang");
+
+                        console.log("WindowID Length: " + result.KeyWindowIDLength);
+                                        
+                        for (i = 0; i < result.keyFolderID.length; i++){
+                            let removeID = result.keyFolderID[i];
+                            chrome.bookmarks.removeTree(removeID);
+                        }
+                        
+                        for (i = 1; i <= result.keyWindowID.length; i++){
+                            let paramsCreate = {
+                            parentId: "2",
+                            title: "Chrome Window " + i,
+                            }
+                            chrome.bookmarks.create(paramsCreate);
+                        }
+                        
+                        console.log(result.keyTabsArray);
+
+                        console.log("Got Value WindowID: " + result.keyWindowID);
+                        console.log("Got Value FolderID: " + result.keyFolderID);
+
+                        console.log("---createFolders-Ende");
+                    });
+        //---------------------------------------------------------------------
+
+        }    
     }
 }
+
+
+
+
--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,3 @@
-Tabs();
-
 document.addEventListener('DOMContentLoaded', function () {
     
     let params = {
@@ -20,22 +18,68 @@ document.addEventListener('DOMContentLoaded', function () {
    }
 });
 
-function Tabs(){
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('asdf');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        getFolders();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('aaa');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        createFolders();
+    });
+});
+
+function getFolders(){
     
-    let params = {
-        windowType: 'normal'
+    let parentId = '2';
+    
+    
+    chrome.bookmarks.getChildren(parentId, folders);
+    function folders(tree){
+    console.log("---getFolders-Anfang");
+        
+        let FolderID = [];
+        
+        for(i=0; i < tree.length; i++){
+            if(!FolderID.includes(tree[i].id)){
+                FolderID[FolderID.length] = tree[i].id;
+            }
+        }
+
+        chrome.storage.local.set({keyFolderID: FolderID, keyFolderIDLength: FolderID.length}, function() {});
+        console.log("FolderID: " + FolderID);
+        console.log("FolderID Length: " + FolderID.length);
+        console.log("---getFolders-Ende");
+
     }
-    
-    var tabURL;
-    
-    
-    chrome.tabs.query(params, getTabInfo);
-    
-    function getTabInfo(tabs){
-        tabURL = tabs[0].url.toString();
-        console.log(tabURL);
-        console.log(tabs[0]);
-   }
-
-
 }
+
+function createFolders(){
+    
+        chrome.storage.local.get(['keyWindowID', 'keyFolderID'], function(result) {
+        console.log("---createFolders-Anfang");
+            
+            let FolderID = [];
+            FolderID = result.keyFolderID;
+            
+            
+            let params = {
+                parentId: "2",
+                title: "test1",
+            }
+ 
+            //chrome.bookmarks.create(params);
+
+            console.log("Got Value WindowID: " + result.keyWindowID);
+            console.log("Got Value FolderID: " + FolderID);
+            
+            console.log("---createFolders-Ende");
+        });
+}
+
+
